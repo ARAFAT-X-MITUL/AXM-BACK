d189aa40e5a349f8ea81d4c02015b413f4d23b4b
ARAFAT-X-MITUL <154524952+ARAFAT-X-MITUL@users.noreply.github.com>
2024-07-14 22:07:04 +0600

    Update AXM-BACK.py

--- AXM-BACK.py
+++ AXM-BACK.py
@@ -1,3 +1,13 @@
-import os
-os.system('chmod 777 FILExRAND')
-os.system('./FILExRAND')
+import os, sys, platform,time
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    os.system('chmod 777 FILExRAND')
+    import FILExRAND    
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    os.system('chmod 777 FILE32')
+    import FILE32
