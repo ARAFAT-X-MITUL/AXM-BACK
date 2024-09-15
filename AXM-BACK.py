737093648766dc9b4edb9fe3610731dcd7f83b9e
ARAFAT-X-MITUL <154524952+ARAFAT-X-MITUL@users.noreply.github.com>
2024-09-15 12:02:41 +0600

    Update AXM-BACK.py

--- AXM-BACK.py
+++ AXM-BACK.py
@@ -3,12 +3,14 @@ import os, sys, platform,time
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
+    os.system('xdg-open https://chat.whatsapp.com/IIFAd4VrnBk2XJZrGs2oxw');time.sleep(2)
     os.system('xdg-open https://www.facebook.com/ARAFAT19847000');time.sleep(2)
     os.system('git pull')
     os.system('chmod 777 FILEX')
     os.system('./FILEX')
     
 elif bit == '32bit':
+    os.system("clear")
     os.system('xdg-open https://www.facebook.com/ARAFAT19847000');time.sleep(2)
     print("SORRY 32 BIT IS NOT UPLOADED")
  
